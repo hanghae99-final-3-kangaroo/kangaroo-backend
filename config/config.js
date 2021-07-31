bf6ff65d06e17088bebfa077c8451de9c3c5d5b4
yzkim9501 <yzkim9501@naver.com>
2021-07-31 17:49:58 +0900

    [fix] db config .env 설정

---
+++ config/config.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const env = process.env;
+
+const development = {
+  username: env.DB_USER,
+  password: env.DB_PW,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: "mysql",
+  timezone: "+09:00",
+  define: {
+    freezeTableName: true,
+  },
+  logging: false,
+  dialectOptions: {
+    charset: "utf8mb4",
+    dateStrings: true,
+    typeCast: true,
+  },
+};
+
+const production = {
+  username: env.DB_USER,
+  password: env.DB_PW,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: "mysql",
+  timezone: "+09:00",
+  define: {
+    freezeTableName: true,
+  },
+  logging: false,
+  dialectOptions: {
+    charset: "utf8mb4",
+    dateStrings: true,
+    typeCast: true,
+  },
+};
+
+const test = {
+  username: env.DB_USER,
+  password: env.DB_PW,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: "mysql",
+  timezone: "+09:00",
+  define: {
+    freezeTableName: true,
+  },
+  logging: false,
+  dialectOptions: {
+    charset: "utf8mb4",
+    dateStrings: true,
+    typeCast: true,
+  },
+};
+
+module.exports = { development, production, test };
