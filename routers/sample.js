ad797cee9296532759388e76d3b5f8b4c2e44cae
yzkim9501 <yzkim9501@naver.com>
2021-08-04 17:59:55 +0900

    [fix] 대학 정보 수정 및 추가

--- routers/sample.js
+++ routers/sample.js
@@ -99,53 +99,53 @@ router.get("/sample3", async (req, res, next) => {
   try {
     //베트남
     await university.create({
-      name: "하노이 국립대",
+      name: "Vietnam National University - Hanoi",
       country_id: 1,
       email_domain: "vnu.edu.vn",
     });
     await university.create({
-      name: "똔득탕 대학교",
+      name: "Ton Duc Thang University",
       country_id: 1,
       email_domain: "tdtu.edu.vn",
     });
     await university.create({
-      name: "호치민 인사대",
+      name: "University of Social Sciences and Humanities Vietnam National University Ho Chi Minh City",
       country_id: 1,
       email_domain: "vns.edu.vn",
     });
     await university.create({
-      name: "호치민 사범대",
+      name: "Ho Chi Minh City University of Education",
       country_id: 1,
       email_domain: "hcmue.edu.vn",
     });
     await university.create({
-      name: "RMIT",
+      name: "RMIT University Vietnam",
       country_id: 1,
       email_domain: "rmit.edu.vn",
     });
     //호주
     await university.create({
-      name: "시드니 대학교",
+      name: "University of Sydney",
       country_id: 2,
       email_domain: "uni.sydney.edu.au",
     });
     await university.create({
-      name: "뉴사우스웨일즈 대학교",
+      name: "University of New South Wales",
       country_id: 2,
       email_domain: "unsw.edu.au",
     });
     await university.create({
-      name: "unsw.edu.au",
+      name: "University of Melbourne",
       country_id: 2,
       email_domain: "student.unimelb.edu.au",
     });
     await university.create({
-      name: "퀸즐랜드 대학교",
+      name: "University of Queensland",
       country_id: 2,
       email_domain: "uqconnect.edu.au",
     });
     await university.create({
-      name: "퀸즐랜드 공과대학",
+      name: "Queensland University of Technology",
       country_id: 2,
       email_domain: "connect.qut.edu.au",
     });
@@ -162,7 +162,7 @@ router.get("/sample3", async (req, res, next) => {
       email_domain: "illinois.edu",
     });
     await university.create({
-      name: "University of California - Berkely",
+      name: "University of California, Berkely",
       country_id: 3,
       email_domain: "ucla.edu",
     });
@@ -177,7 +177,7 @@ router.get("/sample3", async (req, res, next) => {
       email_domain: "columbia.edu",
     });
     await university.create({
-      name: "UCLA",
+      name: "University of California, Los Angeles",
       country_id: 3,
       email_domain: "humnet.ucla.edu",
     });
@@ -224,12 +224,12 @@ router.get("/sample3", async (req, res, next) => {
       email_domain: "mcgill.ca",
     });
     await university.create({
-      name: "The British Columbia University",
+      name: "British Columbia University",
       country_id: 4,
       email_domain: "student.ubc.ca",
     });
     await university.create({
-      name: "The university of Alberta",
+      name: "University of Alberta",
       country_id: 4,
       email_domain: "ualberta.ca",
     });
@@ -240,4 +240,38 @@ router.get("/sample3", async (req, res, next) => {
   }
 });
 
+router.get("/sample4", async (req, res, next) => {
+  try {
+    //영국
+    await university.create({
+      name: "University College London",
+      country_id: 5,
+      email_domain: "ucl.ac.uk",
+    });
+    await university.create({
+      name: "University of London",
+      country_id: 5,
+      email_domain: "student.london.ac.uk",
+    });
+    await university.create({
+      name: "University of the Arts London",
+      country_id: 5,
+      email_domain: "arts.ac.uk",
+    });
+    await university.create({
+      name: "University of Leeds",
+      country_id: 5,
+      email_domain: "leeds.ac.uk",
+    });
+    await university.create({
+      name: "King's College London KCL",
+      country_id: 5,
+      email_domain: "kcl.ac.uk",
+    });
+    res.status(200).send("가상 데이터 생성 성공");
+  } catch (err) {
+    console.error(err);
+    res.status(400).send(err);
+  }
+});
 module.exports = router;
