72f201867dd006ed4bea4aec1722383472affb51
yzkim9501 <yzkim9501@naver.com>
2021-08-10 03:43:46 +0900

    [feat] univBoard like / search  추가

--- routers/univBoard.js
+++ routers/univBoard.js
@@ -2,17 +2,16 @@ const express = require("express");
 
 const { univ_board, univ_comment, user, univ_like } = require("../models");
 const authMiddleware = require("../middlewares/auth-middleware");
-const likeMiddleware = require("../middlewares/like-middleware");
 const { Sequelize } = require("sequelize");
 const router = express.Router(); // 라우터라고 선언한다.
+const { or, like } = Sequelize.Op;
 
 // Post Part
 
 // univ_board 글 작성
 router.post("/post", authMiddleware, async (req, res, next) => {
   try {
-    const { user } = res.locals;
-    const user_id = user.user_id;
+    const { user_id } = res.locals.user;
 
     const { univ_id, title, category, content, is_fixed, img_list } = req.body;
 
@@ -47,10 +46,17 @@ router.post("/post", authMiddleware, async (req, res, next) => {
 });
 
 // univ_board 글 조회
-router.get("/post", async (req, res, next) => {
+router.get("/post", authMiddleware, async (req, res, next) => {
   try {
-    const { pageSize, pageNum, category, univ_id } = req.query;
-
+    const { user_id, univ_id } = res.locals.user;
+    const { pageSize, pageNum, category } = req.query;
+    if (!pageSize || !pageNum) {
+      res.status(403).send({
+        message: "pageSize, pageNum을 입력하세요.",
+        ok: false,
+      });
+      return;
+    }
     let offset = 0;
 
     if (pageNum > 1) {
@@ -59,6 +65,7 @@ router.get("/post", async (req, res, next) => {
 
     const options = {
       subQuery: false,
+      raw: true,
       limit: Number(pageSize),
       order: [["createdAt", "DESC"]],
       offset: offset,
@@ -77,10 +84,29 @@ router.get("/post", async (req, res, next) => {
       group: ["post_id"],
     };
     if (category !== undefined) options.where.category = category;
-    if (univ_id !== undefined) options.where.univ_id = univ_id;
+    options.where.univ_id = univ_id;
 
     const result = await univ_board.findAll(options);
+    for (let i = 0; i < result.length; i++) {
+      let is_like = false;
+      my_like = await univ_like.findOne({
+        where: {
+          user_id,
+          post_id: result[i].post_id,
+        },
+      });
+      if (my_like) {
+        is_like = true;
+      }
 
+      all_like = await univ_like.findAll({
+        where: { post_id: result[i].post_id },
+      });
+      result[i].like = {
+        is_like,
+        all_like: all_like.length,
+      };
+    }
     // let img_list;
     // for (i = 0; i < result.length; i++) {
     //   img_list = result[i]["img_list"];
@@ -105,6 +131,102 @@ router.get("/post", async (req, res, next) => {
   }
 });
 
+router.get("/search", async (req, res, next) => {
+  try {
+    const { univ_id, user_id } = res.locals.user;
+    const { pageSize, pageNum, category, sort } = req.query;
+    if (!pageSize || !pageNum) {
+      res.status(403).send({
+        message: "pageSize, pageNum을 입력하세요.",
+        ok: false,
+      });
+      return;
+    }
+    let { keyword } = req.query;
+
+    keyword = keyword.trim(); //trim으로 앞뒤 공백 제거
+    if (!keyword.length) {
+      //! 키워드에 공백만 존재
+      return res.status(400).json("invalid target");
+    }
+    keyword = keyword.replace(/\s\s+/gi, " "); //target 사이에 공백이 2개 이상 존재 > 하나의 공백으로 변환
+
+    let offset = 0;
+    if (pageNum > 1) {
+      offset = pageSize * (pageNum - 1);
+    }
+
+    const options = {
+      subQuery: false,
+      offset: offset,
+      where: {
+        [or]: [
+          { title: { [like]: `%${keyword}%` } },
+          { content: { [like]: `%${keyword}%` } },
+        ],
+      },
+      limit: Number(pageSize),
+      order: [["createdAt", "DESC"]],
+      attributes: {
+        include: [
+          [Sequelize.fn("COUNT", Sequelize.col("comment_id")), "coment_count"],
+        ],
+      },
+      include: [
+        {
+          model: univ_comment,
+          attributes: [],
+        },
+      ],
+      group: ["post_id"],
+      raw: true,
+    };
+    if (category !== undefined) options.where.category = category;
+    options.where.univ_id = univ_id;
+    const result = await univ_board.findAll(options);
+
+    for (let i = 0; i < result.length; i++) {
+      let is_like = false;
+      my_like = await univ_like.findOne({
+        where: {
+          user_id,
+          post_id: result[i].post_id,
+        },
+      });
+      if (my_like) {
+        is_like = true;
+      }
+
+      all_like = await univ_like.findAll({
+        where: { post_id: result[i].post_id },
+      });
+      result[i].like = {
+        is_like,
+        all_like: all_like.length,
+      };
+    }
+    if (sort == "relative") {
+      for (let i = 0; i < result.length; i++) {
+        let rel = 0;
+        rel += result[i]["title"].split(keyword).length - 1;
+        rel += result[i]["content"].split(keyword).length - 1;
+        result[i]["rel"] = rel;
+      }
+      result.sort((a, b) => b.rel - a.rel); // rel의 값 순으로 내림차순 정렬.sort((a, b) => b.rel - a.rel); // rel의 값 순으로 내림차순 정렬
+    }
+
+    res.status(200).send({
+      result,
+      ok: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).send({
+      ok: false,
+      message: `${err} : 게시글 조회 실패`,
+    });
+  }
+});
 router.get("/post/:post_id/view_count", async (req, res, next) => {
   try {
     const { post_id } = req.params;
@@ -125,7 +247,7 @@ router.get("/post/:post_id/view_count", async (req, res, next) => {
 router.get("/post/:post_id", async (req, res, next) => {
   try {
     const { post_id } = req.params;
-
+    const { user_id } = res.locals.user;
     const result = await univ_board.findOne({
       where: { post_id },
       include: [
@@ -142,6 +264,19 @@ router.get("/post/:post_id", async (req, res, next) => {
       });
       return;
     } else {
+      let is_like = false;
+      my_like = await univ_like.findOne({
+        where: {
+          user_id,
+          post_id,
+        },
+      });
+      if (my_like) {
+        is_like = true;
+      }
+
+      all_like = await univ_like.findAll({ where: { post_id } });
+
       // if (result.img_list != null) {
       //   result.img_list = img_list = result["img_list"].split(",");
       // } else {
@@ -149,6 +284,10 @@ router.get("/post/:post_id", async (req, res, next) => {
       // }
 
       res.status(200).send({
+        like: {
+          is_like,
+          all_like: all_like.length,
+        },
         result,
         ok: true,
       });
@@ -255,6 +394,39 @@ router.delete("/post/:post_id", authMiddleware, async (req, res, next) => {
   }
 });
 
+// univ_board 글 좋아요
+router.get("/post/:post_id/like", authMiddleware, async (req, res, next) => {
+  try {
+    const { user_id } = res.locals;
+    const { post_id } = req.params;
+    const my_like = await univ_like.findOne({
+      where: { post_id, user_id },
+    });
+
+    if (my_like == null) {
+      await univ_like.create({
+        post_id,
+        user_id,
+      });
+      res.status(200).send({
+        message: "liked post",
+        ok: true,
+      });
+    } else {
+      await my_like.destroy();
+      res.status(200).send({
+        message: "disliked post",
+        ok: true,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(400).send({
+      ok: false,
+      message: `${err} : 좋아요/좋아요 취소 실패`,
+    });
+  }
+});
 // Comment Part
 
 // univ_comment 작성
