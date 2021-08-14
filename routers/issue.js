fbf287d8326a574d3ddbd7e10a7d2505b000cfb2
장상현 <81303869+firstquarter-J@users.noreply.github.com>
2021-08-15 02:46:32 +0900

    [fix] DB 조회 raw query 방식으로 변경

--- routers/issue.js
+++ routers/issue.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { free_board, free_comment, free_like, issue } = require("../models");
+const { free_board, issue, sequelize } = require("../models");
 const { Sequelize } = require("sequelize");
 
 const schedule = require("node-schedule");
 
-const Op = Sequelize.Op;
-
-let issue_post_result;
-
 schedule.scheduleJob("31 8 * * * *", async function () {
-  console.log(new Date());
-  console.log("매 시간 8분 31초마다 실행");
+  console.log(new Date().toJSON() + " 마다 갱신");
 
   // 저번 회차 이슈 게시글 삭제
   await issue.destroy({
@@ -20,40 +15,20 @@ schedule.scheduleJob("31 8 * * * *", async function () {
   });
 
   // 현재 시각 기준, 24시간 전까지의 게시글 조회
-  const get_free_post = await free_board.findAll({
-    where: {
-      createdAt: {
-        [Op.gt]: new Date(Date.now() - 60 * 60 * 24 * 1000),
-      },
-    },
-    include: [
-      {
-        model: free_comment,
-      },
-      {
-        model: free_like,
-      },
-    ],
-  });
-
-  // 조회한 게시글마다 조회수, 댓글수, 좋아요수 합산하여 배열로 저장
-  let issue_post = [];
-  let post_id, sum;
-
-  for (let i = 0; i < get_free_post.length; i++) {
-    let issue_obj = {};
-
-    post_id = get_free_post[i]["post_id"];
-    sum =
-      get_free_post[i]["free_likes"].length * 20 +
-      get_free_post[i]["free_comments"].length * 10 +
-      get_free_post[i]["view_count"];
-
-    issue_obj.post_id = post_id;
-    issue_obj.sum = sum;
-
-    issue_post.push(issue_obj);
-  }
+  let issue_post = await sequelize.query(
+    "select free_board.post_id,\
+    free_board.view_count\
+    + count(distinct free_comment.user_id) * 10\
+    + count(distinct free_like.user_id) * 20\
+    as sum from free_board \
+    left join free_comment\
+    on free_board.post_id = free_comment.post_id\
+    left join free_like\
+    on free_board.post_id = free_like.post_id\
+    WHERE free_board.createdAt BETWEEN DATE_ADD(NOW(),INTERVAL -1 DAY ) AND NOW()\
+    group by free_board.post_id;",
+    { type: Sequelize.QueryTypes.SELECT }
+  );
 
   // 새로운 인기 게시글 생성
   await issue.bulkCreate(issue_post, { returning: true });
@@ -62,7 +37,7 @@ schedule.scheduleJob("31 8 * * * *", async function () {
 router.get("/", async (req, res, next) => {
   try {
     // 생성된 인기 게시글 조회
-    issue_post_result = await issue.findAll({
+    let issue_post_result = await issue.findAll({
       limit: 10,
       order: [["sum", "DESC"]],
       include: [{ model: free_board }],
@@ -70,6 +45,17 @@ router.get("/", async (req, res, next) => {
         exclude: ["issue_id", "createdAt", "updatedAt", "sum"],
       },
     });
+
+    let img_list;
+    for (i = 0; i < issue_post_result.length; i++) {
+      img_list = issue_post_result[i]["free_board"]["img_list"];
+      if (img_list != null) {
+        img_list = img_list.split(",");
+      } else {
+        img_list = [];
+      }
+      issue_post_result[i]["free_board"].img_list = img_list;
+    }
 
     res.status(200).send({
       result: issue_post_result,
