3577d98be8af6ad67308e0bc74d0d9a94e44a835
yzkim9501 <yzkim9501@naver.com>
2021-08-20 00:49:37 +0900

    [fix] issue 게시글 3개 이하이면 과거글 가져옴

--- services/issue-service.js
+++ services/issue-service.js
@@ -10,20 +10,33 @@ const destroyIssue = async () => {
 
 // 현재 시각 기준, 24시간 전까지의 게시글 조회
 const calculateIssue = async () => {
-  return await sequelize.query(
-    "select free_board.post_id,\
-        free_board.view_count\
-        + count(distinct free_comment.user_id) * 10\
-        + count(distinct free_like.user_id) * 20\
-        as sum from free_board \
-        left join free_comment\
-        on free_board.post_id = free_comment.post_id\
-        left join free_like\
-        on free_board.post_id = free_like.post_id\
-        WHERE free_board.createdAt BETWEEN DATE_ADD(NOW(),INTERVAL -1 DAY ) AND NOW()\
-        group by free_board.post_id;",
-    { type: Sequelize.QueryTypes.SELECT }
-  );
+  let startDate = new Date();
+  startDate.setDate(startDate.getDate() - 1);
+  let endDate = new Date();
+  endDate.setDate(endDate.getDate() - 7);
+  let result = [];
+  while (result.length < 4) {
+    result = await sequelize.query(
+      `select free_board.post_id,
+          free_board.view_count
+          + count(distinct free_comment.user_id) * 10
+          + count(distinct free_like.user_id) * 20
+          as sum from free_board 
+          left join free_comment
+          on free_board.post_id = free_comment.post_id
+          left join free_like
+          on free_board.post_id = free_like.post_id
+          WHERE free_board.createdAt BETWEEN '${startDate
+            .toISOString()
+            .slice(0, 19)
+            .replace("T", " ")}' AND NOW()
+          group by free_board.post_id;`,
+      { type: Sequelize.QueryTypes.SELECT }
+    );
+    startDate.setHours(startDate.getHours() - 1);
+    if (startDate < endDate) break;
+  }
+  return result;
 };
 
 // 새로운 인기 게시글 생성
